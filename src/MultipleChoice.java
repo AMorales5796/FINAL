7d6509f1e6670c52a088aff7cd08ad84efa22219
Alexyss Morales <91086831@mvusd.net>
2019-12-10 14:00:01 -0800

    Initial commit

---
+++ src/MultipleChoice.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class MultipleChoice
+{
+    String[] questions = {"A","A-","B+","B","B-","C+","C","C-","D+","D","D-","F"};
+    int[] correctAnswers = {95,90,87,83,80,77,73,70,67,63,60,0};
+}//end of class
